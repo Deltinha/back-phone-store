e9b09e20c375391c09a246eac38fa2e536da1a8d
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-09 21:14:55 -0300

    feat/ conditional database setting

--- database/database.js
+++ database/database.js
@@ -4,12 +4,22 @@ const { Pool } = pg;
 
 console.log(`Using the database ${process.env.DB_DATABASE}.`);
 
-const databaseConfig = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+let databaseConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
 };
+
+if (process.env.DATABASE_URL) {
+  databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
 
 const connection = new Pool(databaseConfig);
 
