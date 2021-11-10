4ff68631c0b3a79698f62c80d4bde36cafe87f63
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-09 23:06:57 -0300

    fix/ trailing comma

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-import app from "./app";
+import app from './app';
 
-process.on("unhandledRejection", (reason, promise) => {
+process.on('unhandledRejection', (reason, promise) => {
   console.error(
-    `Server exiting due to an unhandled promise rejection: ${promise} and reason ${reason}`
+    `Server exiting due to an unhandled promise rejection: ${promise} and reason ${reason}`,
   );
   throw reason;
 });
 
-process.on("uncaughtException", (error) => {
-  console.error("Server exiting due to uncaught exception", error);
+process.on('uncaughtException', (error) => {
+  console.error('Server exiting due to uncaught exception', error);
 });
 
 app.listen(process.env.PORT, () => {
