3574d007bd4f0a14aa2a9b12dd575a8ae423c1fe
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-11 03:13:12 -0300

    fix/ get product by id query

--- src/repositories/product.js
+++ src/repositories/product.js
@@ -4,8 +4,7 @@ import connection from '../database/database.js';
 export async function getProductInfoById(id) {
   const product = await connection.query(
     `
-  SELECT * FROM products
-  WHERE id = $1;
+    SELECT final_result.*, product_image.url, products.value FROM crosstab('SELECT product_category.product_id, categories.type, categories.name FROM product_category JOIN categories ON category_id=categories.id ORDER BY 1,2') AS final_result(id INTEGER, brand TEXT, capacity TEXT, color TEXT, model TEXT) JOIN product_image ON final_result.id=product_image.product_id JOIN products ON final_result.id=products.id WHERE product_image.perspective='front' AND final_result.id=$1;
   `,
     [id],
   );
