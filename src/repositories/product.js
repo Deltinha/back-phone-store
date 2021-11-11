7edb3a2e3681ae5215ff20235d1e66adb4a7db17
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-11 01:13:20 -0300

    fix/ updated query to fit new db model

--- src/repositories/product.js
+++ src/repositories/product.js
@@ -14,7 +14,7 @@ export async function getProductInfoById(id) {
 
 export async function getAllProducts() {
   const products = await connection.query(`
-  SELECT products.*, product_image.url as "imageUrl" FROM products JOIN product_image ON product_image."product_id"=products."id" WHERE "perspective"='front';
+  SELECT products.*, product_image.url as "imageUrl", categories."type", categories."name" FROM products JOIN product_image ON product_image."product_id"=products."id" JOIN product_category ON product_category."product_id"=products."id" JOIN categories ON categories."id"=product_category."category_id";
   `);
   return products.rows;
 }
