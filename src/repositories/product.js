460a16c67618ae4d45f60121d3bfc698b75c1592
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-11 02:25:52 -0300

    fix/ duplicated products

--- src/repositories/product.js
+++ src/repositories/product.js
@@ -14,7 +14,7 @@ export async function getProductInfoById(id) {
 
 export async function getAllProducts() {
   const products = await connection.query(`
-  SELECT products.*, product_image.url as "imageUrl", categories."type", categories."name" FROM products JOIN product_image ON product_image."product_id"=products."id" JOIN product_category ON product_category."product_id"=products."id" JOIN categories ON categories."id"=product_category."category_id";
+  SELECT final_result.*, product_image, products.value FROM crosstab('SELECT product_category.product_id, categories.type, categories.name FROM product_category JOIN categories ON category_id=categories.id ORDER BY 1,2') AS final_result(id INTEGER, brand TEXT, capacity TEXT, color TEXT, model TEXT) JOIN product_image ON final_result.id=product_image.product_id JOIN products ON final_result.id=products.id WHERE product_image.perspective='front';
   `);
   return products.rows;
 }
