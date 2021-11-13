4003f4fb7d69d84839eeb51cbdfacc01fe50e7d4
coelhothay <thay.csb@gmail.com>
2021-11-13 18:38:09 -0300

    fix/ upate create user schema

--- src/schemas/createUserSchema.js
+++ src/schemas/createUserSchema.js
@@ -4,9 +4,10 @@ export default function validalidateUserInfo(obj) {
   const schema = Joi.object({
     firstName: Joi.string().min(3).required(),
     lastName: Joi.string().min(3).required(),
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: false } }).required(),
+    email: Joi.string().email().required(),
     password: Joi.string().min(6).max(15).required(),
     cep: Joi.string().required().length(8),
+    complement: Joi.string(),
     state: Joi.string().required(),
     city: Joi.string().required(),
     neighborhood: Joi.string().required(),
