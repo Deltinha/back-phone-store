ff349f929efb66c3f4275edc3f150bedc26dfd32
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-14 03:08:39 -0300

    feat/ validation of bearer token

--- src/controllers/checkout.js
+++ src/controllers/checkout.js
@@ -3,7 +3,13 @@ import * as checkoutService from '../services/checkout';
 
 export async function postCheckout(req, res) {
   const cart = req.body;
-  const isCartValid = checkoutService.checkIsCartValid(cart);
-  if (!isCartValid) return res.sendStatus(422);
-  res.sendStatus(201);
+  const auth = req.headers.authorization;
+
+  const isAuthValid = await checkoutService.checkIsAuthValid(auth);
+  if (!isAuthValid) return res.sendStatus(401);
+
+  const isCartValid = await checkoutService.checkIsCartValid(cart);
+  if (!isCartValid) return res.sendStatus(400);
+
+  return res.send(req.headers).status(200);
 }
--- src/services/checkout.js
+++ src/services/checkout.js
@@ -1,7 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 import validadeCheckout from '../schemas/checkoutSchema';
+import validateBearerToken from '../schemas/authorizationSchema';
 
 export async function checkIsCartValid(cart) {
-  const isValidCart = validadeCheckout(cart);
-  return isValidCart;
+  const isCartValid = validadeCheckout(cart);
+  return isCartValid;
+}
+
+export async function checkIsAuthValid(bearerToken) {
+  if (bearerToken === undefined) return false;
+  const isAuthValid = validateBearerToken(bearerToken);
+  return isAuthValid;
 }
